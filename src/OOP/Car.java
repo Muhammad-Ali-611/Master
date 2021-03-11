46d40c0da8a4b9eacd197c31ed59bc3456a8018a
MuhammadAli611 <chma616@gmail.com>
2021-03-11 14:11:40 -0500

    Initial commit

---
+++ src/OOP/Car.java
@@ -0,0 +1,11 @@
+package OOP;
+
+public class Car {
+
+    public static void main(String[] args) {
+        Car Volvo = new Car();
+        Car Toyota = new Car();
+        System.out.println("Toyota");
+        System.out.println("Volvo");
+    }
+}
