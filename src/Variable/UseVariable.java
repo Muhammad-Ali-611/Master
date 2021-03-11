80210b37ee15f93dee187d654c983275fe5c01e1
MuhammadAli611 <chma616@gmail.com>
2021-03-11 14:21:53 -0500

    Variable

---
+++ src/Variable/UseVariable.java
@@ -0,0 +1,12 @@
+package Variable;
+
+public class UseVariable {
+
+    int x = 10;
+
+    public static void main(String[] args) {
+        UseVariable x = new UseVariable();
+        x .x=25;
+        System.out.println("UseVariable.x");
+    }
+}
