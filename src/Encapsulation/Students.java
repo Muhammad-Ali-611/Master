e97d2b6cec4bee99726468d64e85af6fac359023
MuhammadAli611 <chma616@gmail.com>
2021-03-11 14:59:22 -0500

    Encapsulation

---
+++ src/Encapsulation/Students.java
@@ -0,0 +1,38 @@
+package Encapsulation;
+
+public class Students {
+
+    private String RollNum;
+    private String name;
+    private int ID;
+
+    public String getRollNum() {
+        return RollNum;
+    }
+
+    public void setRollNum(String rollNum) {
+        RollNum = rollNum;
+    }
+
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+}
---
+++ src/Encapsulation/TestStudents.java
@@ -0,0 +1,9 @@
+package Encapsulation;
+
+public class TestStudents {
+    public static void main(String[] args) {
+        Students RollNum = new Students();
+        RollNum.setRollNum("26");
+        System.out.println(RollNum.getRollNum());
+    }
+}
